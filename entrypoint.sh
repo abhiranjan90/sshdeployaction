d7e4cdc9618a0a998597e93a01af9f9054b69116
abhiranjan90 <82633990+abhiranjan90@users.noreply.github.com>
2024-05-07 19:09:37 +0530

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Inputs from the action.yaml file
+HOST="$INPUT_HOST"
+SSH_PORT="$INPUT_SSH_PORT"
+SSH_USERNAME="$INPUT_SSH_USERNAME"
+SSH_KEY="$INPUT_SSH_KEY"
+SSH_PASSPHRASE="$INPUT_SSH_PASSPHRASE"
+SOURCE_PATH="$INPUT_SOURCE_PATH"
+DESTINATION_DIR="$INPUT_DESTINATION_DIR"
+REMOVE_DIR="$INPUT_RM"
+
+
+
+# Create an SSH key file
+mkdir -p $HOME/.ssh
+echo "$SSH_KEY" > $HOME/.ssh/id_rsa
+chmod 600 $HOME/.ssh/id_rsa
+
+# Create a passphrase file
+echo "$SSH_PASSPHRASE" > $HOME/.ssh/passphrase
+chmod 600 $HOME/.ssh/id_rsa
+
+# Add the key with passphrase to the SSH agent
+eval $(ssh-agent -s)
+ssh-add $HOME/.ssh/id_rsa < $HOME/.ssh/passphrase
+
+
+# Check if the remove flag is set
+if [ "${REMOVE_DIR}" = true ]; then
+    ssh -p "${SSH_PORT}" -i "${SSH_KEY}" -o "StrictHostKeyChecking=no" "${SSH_USERNAME}@${HOST}" "rm -rf ${DESTINATION_DIR}/*"
+fi
+
+
+
+scp -i $HOME/.ssh/id_rsa -P "${SSH_PORT}" -o StrictHostKeyChecking=no -r $GITHUB_WORKSPACE/${SOURCE_PATH}/* "${SSH_USERNAME}@${HOST}:${DESTINATION_DIR}"
